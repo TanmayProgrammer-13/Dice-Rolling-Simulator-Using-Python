3aad10c63fe78c5ccb9e848b6eb1f0f8b62806c8
TanmayProgrammer-13 <88921795+TanmayProgrammer-13@users.noreply.github.com>
2021-10-11 02:50:53 -0700

    Add files via upload
    Dice Rolling Python

---
+++ Dice Rolling Simluator.py
@@ -0,0 +1,14 @@
+import random
+
+again = True
+
+while again:
+    print(random.randint(1,6))
+    another_roll = input("Want To Roll The Dice Again? (Y/N): ")
+    if another_roll.lower() == "y":
+        continue
+    else:
+        break
+
+
+
